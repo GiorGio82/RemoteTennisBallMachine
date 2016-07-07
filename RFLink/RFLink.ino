495ef71468369c677a5c0bf04832302e85c0e419
Giorgio Corbellini <giorgio@disneyresearch.com>
2016-07-07 18:01:57 +0200

    tx and rx firmwares

--- RFLink/RFLink.ino
+++ RFLink/RFLink.ino
@@ -14,11 +14,8 @@
 // Arduino digital pins
 #define BUTTON_PIN  2
 #define LED_PIN  13
-#define RX_PIN  12
+#define RX_PIN  7
 
-// Button hardware is setup so the button goes LOW when pressed
-#define BUTTON_PRESSED LOW
-#define BUTTON_NOT_PRESSED HIGH
 
 void setup()
 {
@@ -32,54 +29,48 @@ void setup()
   digitalWrite(LED_PIN, LOW);
 
   Serial.begin(1200);  // Hardware supports up to 2400, but 1200 gives longer range
-  //Serial.println("Start");
 }
 
 #ifdef TRANSMITTER
 void loop()
 {
-  static int prev_button = BUTTON_NOT_PRESSED;  // Previous button press value
-  int        cur_button;                        // Current button press value
 
-  cur_button = digitalRead(BUTTON_PIN);
-  
-  if ((prev_button == BUTTON_NOT_PRESSED) && (cur_button == BUTTON_PRESSED))
-  {
-   
-    
-  }
-
-  delay(500);
-  writeUInt(158); // Put any number you want to send here (71 is just a test)
-  //delay(50); // Debounce button
-  //prev_button = cur_button;
-  delay(500);
+  writeUInt(158); // Put any number you want to send here
+  digitalWrite(LED_PIN, HIGH);
   
 }
 #endif //TRANSMITTER
 
 #ifdef RECEIVER
+int cnt = 0;
+int threshold = 20;
 void loop()
 {
   boolean light_led = false;
 
   
-  if (readUInt(true) == 158) // Check to see if we got the 71 test number
+  if (readUInt(true) == 158) // Check to see if we got the test number
   {
-    light_led = true;
-    digitalWrite(LED_PIN, HIGH);
-    //Serial.println("rx");
+    
+    cnt++;
+ 
   }
-  digitalWrite(LED_PIN, LOW);
 
+  if(cnt >= threshold){
+    
+    cnt = 0;
+    light_led = true;
+    
+  }
   
   if (light_led)
   {
     light_led = false;
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
     digitalWrite(RX_PIN, HIGH);
     delay(50);
     digitalWrite(RX_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
   }
   
 }
